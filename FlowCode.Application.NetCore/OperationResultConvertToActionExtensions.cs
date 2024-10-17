8f8573d54335f195202efed597289efbc66e1193
Robin.Holzner <robin.holzner@epinova.no>
2024-10-17 21:01:19 +0200

    Add FlowCode.Application.NetCore project and extension methods
    A new project named `FlowCode.Application.NetCore` has been added to the solution, targeting .NET 9.0 with implicit usings and nullable reference types enabled. This project includes a package reference to `Microsoft.AspNetCore.Mvc.Core` version `2.2.5` and references the existing `FlowCode.Application` project. Solution configurations have been updated to include build configurations for the new project in both Debug and Release modes.
    
    Additionally, a new file `OperationResultConvertToActionExtensions.cs` has been added, containing extension methods for converting `OperationResult<T>` to `ActionResult` or `IActionResult` using provided success and error functions. These methods handle the success and error scenarios based on the state of the `OperationResult`.

---
+++ FlowCode.Application.NetCore/OperationResultConvertToActionExtensions.cs
@@ -0,0 +1,57 @@
+﻿using FlowCode;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSK.Optimizely.Site.Infrastructure.FlowCode;
+
+public static class OperationResultConvertToActionExtensions
+{
+    /// <summary>
+    /// Converts an <see cref="OperationResult{T}"/> to a <see cref="Result"/> using the provided functions.
+    /// </summary>
+    /// <typeparam name="Result"></typeparam>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="operationResult"></param>
+    /// <param name="success"></param>
+    /// <param name="error"></param>
+    /// <returns></returns>
+    public static ActionResult ToAction<T>(this OperationResult<T> operationResult, Func<T, ActionResult> success, Func<Exception, ActionResult> error)
+    {
+        if (operationResult.IsSuccess)
+        {
+            if (operationResult.Data is null)
+            {
+                return error(new OperationResultException("Data is null"));
+            }
+
+            return success(operationResult.Data);
+        }
+
+        if (operationResult.Exception is null)
+        {
+            return error(new OperationResultException("Exception is null"));
+        }
+
+        return error(operationResult.Exception);
+    }
+
+    public static IActionResult ToAction<T>(this OperationResult<T> operationResult, Func<T, IActionResult> success, Func<Exception, IActionResult> error)
+    {
+        if (operationResult.IsSuccess)
+        {
+            if (operationResult.Data is null)
+            {
+                return error(new OperationResultException("Data is null"));
+            }
+
+            return success(operationResult.Data);
+        }
+
+        if (operationResult.Exception is null)
+        {
+            return error(new OperationResultException("Exception is null"));
+        }
+
+        return error(operationResult.Exception);
+    }
+}
+
